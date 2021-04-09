ac8bc7aaff9f938aab2cba56599f1940316b8dbb
Andrew Brogdon <redbrogdon@google.com>
2021-04-09 14:32:29 -0700

    Simplified

--- lib/main.dart
+++ lib/main.dart
@@ -23,35 +23,36 @@ class MyHomePage extends StatelessWidget {
       ),
       body: Center(
         child: OverflowBox(
-          minHeight: 300,
-          maxHeight: 300,
+          minHeight: 200,
+          maxHeight: 200,
           minWidth: 200,
           maxWidth: 200,
-          child: Stack(
-            overflow: Overflow.visible,
-            children: [
-              Positioned(
-                top: 100,
-                left: 100,
-                child: ColorBlock(Colors.blue, 100, 100),
-              ),
-              Positioned(
-                top: 50,
-                left: 150,
-                child: ColorBlock(Colors.red, 120, 100),
-              ),
-              Positioned(
-                top: 0,
-                left: 0,
-                child: ColorBlock(Colors.green, 100, 120),
-              ),
-            ],
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              border: Border.all(),
+            ),
+            child: ColorStack(),
           ),
         ),
       ),
     );
   }
 }
+
+class ColorStack extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      overflow: Overflow.visible,
+      children: [
+        PositionedColorBlock(Colors.blue, 90, 75),
+        PositionedColorBlock(Colors.red, 50, 150),
+        PositionedColorBlock(Colors.green, 0, 0),
+      ],
+    );
+  }
+}
+
 class MyHomePage1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -90,6 +91,7 @@ class MyHomePage1 extends StatelessWidget {
     );
   }
 }
+
 class MyHomePage2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -128,6 +130,7 @@ class MyHomePage2 extends StatelessWidget {
     );
   }
 }
+
 class MyHomePage3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -166,6 +169,7 @@ class MyHomePage3 extends StatelessWidget {
     );
   }
 }
+
 class MyHomePage4 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -204,6 +208,7 @@ class MyHomePage4 extends StatelessWidget {
     );
   }
 }
+
 class MyHomePage5 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -242,6 +247,7 @@ class MyHomePage5 extends StatelessWidget {
     );
   }
 }
+
 class MyHomePage6 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -280,6 +286,7 @@ class MyHomePage6 extends StatelessWidget {
     );
   }
 }
+
 class MyHomePage7 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -318,6 +325,7 @@ class MyHomePage7 extends StatelessWidget {
     );
   }
 }
+
 class MyHomePage8 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -370,6 +378,27 @@ class ColorBlock extends StatelessWidget {
       width: width,
       height: height,
       color: color,
+    );
+  }
+}
+
+class PositionedColorBlock extends StatelessWidget {
+  final double top;
+  final double left;
+  final Color color;
+
+  PositionedColorBlock(
+    this.color,
+    this.top,
+    this.left,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: top,
+      left: left,
+      child: ColorBlock(color, 100, 100),
     );
   }
 }
